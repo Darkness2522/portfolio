c0c16fc464852f184d431ca4667d7e619ddb33db
Hussan Al Mukhtar <hmm2522@gmail.com>
2026-03-26 22:29:48 +0300

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,23 @@
+// Typing Effect
+const text = "Computer Science Student | Software Developer | Front-End Developer";
+let i = 0;
+
+function typing() {
+  if (i < text.length) {
+    document.querySelector(".typing").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(typing, 50);
+  }
+}
+
+typing();
+
+// Smooth Scroll
+document.querySelectorAll("a").forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
